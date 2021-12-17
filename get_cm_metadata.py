e4ae458e4f8bcba0f2a266227bbbec9f889cae56
admin-hp <titovds.py@yandex.ru>
2021-12-17 17:45:39 +0000

    get candy machine meta

--- get_cm_metadata.py
+++ get_cm_metadata.py
@@ -1,63 +1,70 @@
 import base64
 import base58
 from solana.rpc.api import Client
-from datetime import datetime
 
 
 class GetMeta:
     def __init__(self):
         sol_client = Client("https://api.mainnet-beta.solana.com")
-        account = 'Cu9hWTmbPDBJn3YvqQMGnWsSv5JPd2JrF8ZenFoB6VG7'
+        account = 'DBLJ2VKwKjSnAqPD6MbJQzWSmoEaCLzZ2kwNcDDQwgWv'  # 'DBLJ2VKwKjSnAqPD6MbJQzWSmoEaCLzZ2kwNcDDQwgWv' Cu9hWTmbPDBJn3YvqQMGnWsSv5JPd2JrF8ZenFoB6VG7
         response = sol_client.get_account_info(account, encoding="jsonParsed")
         self.candy_machine = {
-            "authority": None,
-            "wallet": None,
-            "token_mint": None,
-            "config": None,
-            "data": {
-                "uuid": None,
-                "price": None,
-                "items_available": None,
-                "go_live_date": None
-                    },
-            "items_redeemed": None,
-            "bump": None
+            "descriptor": 8,
+            "authority": 32,
+            "wallet": 32,
+            "token_mint": 1,
+            "config": 32,
+            "uuid": 10,
+            "price": 8,
+            "items_available": 8,
+            "go_live_date": 1,
+            "items_redeemed": 8,
+            "bump": 1
             }
         self.decode_metadata(response)
 
     def decode_metadata(self, response):
         decode_data = base64.b64decode(response['result']['value']['data'][0])
-        descriptor = base58.b58encode(decode_data[:8])
-        authority = base58.b58encode(decode_data[8:40])
-        wallet = base58.b58encode(decode_data[40:40+32])
-        mint_index = base58.b58encode(decode_data[40+32:40+32+1])
-        if mint_index == b'1':
-            config = base58.b58encode(decode_data[40+32+1:40+32+1+32])
-            token_mint = None
-        else:
-            token_mint = base58.b58encode(decode_data[40+32+1:40+32+1+32])
+        decode_data = decode_data.hex().rstrip('0')
+        offset = 0
+        for param in self.candy_machine:
+            value = decode_data[offset:self.candy_machine[param] * 2 + offset]
+            # Except options
+            if self.candy_machine[param] == 1:
+                if value == '00':
+                    pass
 
-        stand_data = decode_data[40+32+1+32:40+32+1+32+44]
+                elif param == "token_mint":
+                    self.candy_machine[param] = 32
+                    offset = offset + 2
+                    value = decode_data[offset:self.candy_machine[param] * 2 + offset]
 
-        bump = int(stand_data[-1:].hex(), 16)
-        items_redeemed = int(bytes.fromhex(stand_data[-9:-1].hex().rstrip('0'))[::-1].hex(), 16)
-        go_live_date = int(bytes.fromhex(stand_data[-9-8:-9].hex().rstrip('0'))[::-1].hex(), 16)
-        go_live_date = datetime.utcfromtimestamp(go_live_date).strftime('%d-%m-%Y %H:%M:%S')
-        items_available = int(bytes.fromhex(stand_data[-9-9-8:-9-9].hex().rstrip('0'))[::-1].hex(), 16)
-        price = int(bytes.fromhex(stand_data[-9-9-8-8:-9-9-8].hex().rstrip('0'))[::-1].hex(), 16)
-        uuid = stand_data[:-9-9-8-8][3:][1:].decode('UTF-8')
+                elif param == "go_live_date":
+                    self.candy_machine[param] = 8
+                    offset = offset+2
+                    value = decode_data[offset:self.candy_machine[param] * 2 + offset]
+                # Bump
+                elif param == "bump":
+                    value = int(bytes.fromhex(value).rstrip(b'0')[::-1].hex(), 16)
 
-        self.candy_machine["authority"] = authority
-        self.candy_machine["wallet"] = wallet
-        self.candy_machine["token_mint"] = token_mint
-        self.candy_machine["config"] = config
-        self.candy_machine["data"]["uuid"] = uuid
-        self.candy_machine["data"]["price"] = price
-        self.candy_machine["data"]["items_available"] = items_available
-        self.candy_machine["data"]["go_live_date"] = go_live_date
-        self.candy_machine["items_redeemed"] = items_redeemed
-        self.candy_machine["bump"] = bump
+            # Public keys
+            if self.candy_machine[param] == 32:
+                value = base58.b58encode(bytes.fromhex(value))
 
+            # Int values
+            if self.candy_machine[param] == 8:
+                # Except descriptor (not int)
+                if param == 'descriptor':
+                    value = base58.b58encode(bytes.fromhex(value))
+                else:
+                    value = int(bytes.fromhex(value).rstrip(b'0')[::-1].hex(), 16)
+
+            # uuid
+            if self.candy_machine[param] == 10:
+                value = bytes.fromhex(value)[3:][1:].decode('UTF-8')
+
+            offset = self.candy_machine[param]*2+offset
+            self.candy_machine[param] = value
         print(self.candy_machine)
 
 
--- rarity.py
+++ rarity.py
@@ -17,15 +17,15 @@ class TokenRating:
         projects_file = json.load(open('test.json', 'r'))
         for project in projects_file:
             config_account_dict = {
-                'project_name': None,
-                'project_url': None,
+                'project_name': 'no_name',
+                'project_url': 'no_url',
                 'candy machine': project['candy machine'],
                 'config': project['config'],
                 'items_redeemed': project['items_redeemed'],
                 'items_available': project['items_available'],
                 'price': project['price'],
                 'go_live_date': project['go_live_date'],
-                'rarity_tokens': None}
+                'rarity_tokens': 'no_tokens'}
             self.node_request(config_account_dict)
         with open(f'nft_projects.json', 'w') as outfile:  # запись атрибутов в  json файл
             json.dump(self.end_list, outfile, ensure_ascii=False, indent=4)
@@ -34,8 +34,17 @@ class TokenRating:
     def node_request(self, config_account_dict):
         url_list = []
         response = self.sol_client.get_account_info(config_account_dict['config'], encoding="jsonParsed")
-        decode_data = str(base64.b64decode(response['result']['value']['data'][0]))
+        try:
+            decode_data = str(base64.b64decode(response['result']['value']['data'][0]))
+        except KeyError:
+            if response['error']:
+                config_account_dict['error'] = response['error']
+                self.end_list.append(config_account_dict)
+                print(config_account_dict)
+                return
+
         splitted_data = decode_data.split('https')
+
         for line in splitted_data:
             source = re.search('://(.+?)/', line)
             if source:
@@ -58,12 +67,21 @@ class TokenRating:
             for line in uri_list:
                 tasks.append(asyncio.ensure_future(self.url_request(self, session=session, url=line)))
             #  получение атрибутов из ссылок
-            request = await asyncio.gather(*tasks)
-            project_name = request[0]['symbol']
-            project_url = request[0]['external_url']
-            config_account_dict['project_name'] = project_name
-            config_account_dict['project_url'] = project_url
-            with open(f'{project_name}.json', 'w') as outfile:  # запись атрибутов в  json файл
+            try:
+                request = await asyncio.gather(*tasks)
+            except aiohttp.client_exceptions.ClientConnectorError:
+                print(f'bad url = {line}')
+                self.end_list.append(config_account_dict)
+                return
+            if not len(request[0]['symbol']) == 0:
+                project_name = request[0]['symbol']
+                config_account_dict['project_name'] = project_name
+            try:
+                project_url = request[0]['external_url']
+                config_account_dict['project_url'] = project_url
+            except KeyError:
+                pass
+            with open(f'{config_account_dict["project_name"]}.json', 'w') as outfile:  # запись атрибутов в  json файл
                 json.dump(request, outfile, ensure_ascii=False, indent=4)
         self.sorting_values_for_types(config_account_dict)
 
@@ -122,32 +140,35 @@ class TokenRating:
             token_index += 1
             lis = []
             attribute_count = len(token['attributes'])
-            for attributes in token['attributes']:
-                for trait_type in proc_dic:
-                    if attributes['trait_type'] == trait_type:
-                        for key in proc_dic[trait_type]:
-                            if attributes['value'] == key:
-                                rarity = proc_dic[trait_type][key]
-                                lis.append(rarity)
-            rarity_tokens[str((token['name'], token_index))] = '%.2f' % (sum(lis)/attribute_count)
-        sort_keys = sorted(rarity_tokens, key=rarity_tokens.__getitem__, reverse=True)
-        sort_rarity_tokens = {}
-        for token_name in sort_keys:
-            for key in rarity_tokens:
-                if token_name == key:
-                    sort_rarity_tokens[token_name] = rarity_tokens[key]
-        top_list = []
-        top_rarity_tokens = {}
-        for key in sort_rarity_tokens:
-            top_list.append(key)
-        if len(top_list) >= 3:
-            top_list = top_list[:3]
-            for line in top_list:
+            if not attribute_count == 0:
+                for attributes in token['attributes']:
+                    for trait_type in proc_dic:
+                        if attributes['trait_type'] == trait_type:
+                            for key in proc_dic[trait_type]:
+                                if attributes['value'] == key:
+                                    rarity = proc_dic[trait_type][key]
+                                    lis.append(rarity)
+                rarity_tokens[str((token['name'], token_index))] = '%.2f' % (sum(lis)/attribute_count)
+                sort_keys = sorted(rarity_tokens, key=rarity_tokens.__getitem__, reverse=True)
+                sort_rarity_tokens = {}
+                for token_name in sort_keys:
+                    for key in rarity_tokens:
+                        if token_name == key:
+                            sort_rarity_tokens[token_name] = rarity_tokens[key]
+                top_list = []
+                top_rarity_tokens = {}
                 for key in sort_rarity_tokens:
-                    if line == key:
-                        top_rarity_tokens[line] = sort_rarity_tokens[key]
+                    top_list.append(key)
+                if len(top_list) >= 3:
+                    top_list = top_list[:3]
+                    for line in top_list:
+                        for key in sort_rarity_tokens:
+                            if line == key:
+                                top_rarity_tokens[line] = sort_rarity_tokens[key]
+                    config_account_dict['rarity_tokens'] = top_rarity_tokens
+                else:
+                    config_account_dict['rarity_tokens'] = top_list
 
-        config_account_dict['rarity_tokens'] = top_rarity_tokens
         self.end_list.append(config_account_dict)
         print(config_account_dict)
 
